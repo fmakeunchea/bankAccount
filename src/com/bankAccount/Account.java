e24cc52f31dd551c52bfe9117460041d6d6db741
Fifi Makeunchea <fmakeunch@gmail.com>
2020-07-11 18:20:13 -0400

    Add a Constructor to a class

--- src/com/bankAccount/Account.java
+++ src/com/bankAccount/Account.java
@@ -3,6 +3,12 @@ package com.bankAccount;
 public class Account {
 	private String name; //instance variable
 	
+	// constructor initializes name with parameter name
+	public Account(String name) //constructor name is class name
+	{
+		this.name = name;
+	}
+	
 	// method to set the name in the object
 	public void setName(String name)
 	{
--- src/com/bankAccount/AccountTest.java
+++ src/com/bankAccount/AccountTest.java
@@ -5,7 +5,7 @@ public class AccountTest {
 
 	public static void main(String[] args) {
 		// create a Scanner object to obtain input from the command window
-		Scanner input = new Scanner(System.in);
+	/*	Scanner input = new Scanner(System.in);
 		
         //create an Account object and assign it to myAccount
 		Account myAccount = new Account();
@@ -20,7 +20,15 @@ public class AccountTest {
 		System.out.println();  //output a blank line
 		
 		//display the name store in object myAccount
-		System.out.printf("Name in object myAccount is : %n%s%n", myAccount.getName());
+		System.out.printf("Name in object myAccount is : %n%s%n", myAccount.getName());*/
+		
+		//create two Account objects
+		Account account1 = new Account("Jane Green");
+		Account account2 = new Account("John Blue");
+		
+		//display initial value of name for each Account
+		System.out.printf("sccount1 name is : %s%n", account1.getName());
+		System.out.printf("sccount2 name is : %s%n", account2.getName());
 		}
 
 }// end class AccountTest
