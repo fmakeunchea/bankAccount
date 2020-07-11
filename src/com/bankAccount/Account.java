8064eef67a4760680d6c0717f2aa6e68221af515
Fifi Makeunchea <fmakeunch@gmail.com>
2020-07-11 17:03:12 -0400

    Adding 2 classes Account and AccountTest

---
+++ src/com/bankAccount/Account.java
@@ -0,0 +1,17 @@
+package com.bankAccount;
+
+public class Account {
+	private String name; //instance variable
+	
+	// method to set the name in the object
+	public void setName(String name)
+	{
+		this.name = name; //store the name
+	}
+	
+	//method to retrieve the name from the object
+    public String getName()
+    {
+    	 return name;  //return value of name to caller
+    }
+}// end class Account
